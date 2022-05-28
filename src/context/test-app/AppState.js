116b2bf872ea1259b17285cc389fde25374bd940
GavrilS <gavr1lll@abv.bg>
2022-05-28 14:33:42 +0000

    Added searchItem component

--- src/context/test-app/AppState.js
+++ src/context/test-app/AppState.js
@@ -7,18 +7,28 @@ import {
 
 const AppState = props => {
     const initialState = {
-
+        searchItem: {},
+        itemCollection: []
     }
 
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
     // component actions here!!!
+    const getSearchItem = () => {
 
+    };
+
+    const getItemCollection = () => {
+
+    };
 
 
     return <AppContext.Provider
         value={{
-
+            searchItem: state.searchItem,
+            itemCollection: state.itemCollection,
+            getSearchItem,
+            getItemCollection
         }}
     >
         {props.children}
