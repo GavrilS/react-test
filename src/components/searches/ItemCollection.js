2485ac69dfbbd41bdefa11818265484421deeac6
GAVRIL SAMODIVEKOV <gsamodivekov@gmail.com>
2022-05-31 21:07:18 +0300

    Added itemCard and itemCollection functional components

---
+++ src/components/searches/ItemCollection.js
@@ -0,0 +1,29 @@
+import React, { useContext, useEffect } from "react";
+import ItemCard from './ItemCard';
+import AppContext from "../../context/test-app/AppContext";
+
+const ItemCollection = () => {
+    const appContext = useContext(AppContext);
+
+    const { itemCollection, getItemCollection } = appContext;
+
+    useEffect(() => {
+        getItemCollection();
+    }, []);
+
+    return(
+        <div style={itemStyle}>
+            {itemCollection.map(searchItem => (
+                <ItemCard key={searchItem.id} searchItem={searchItem} />
+            ))}
+        </div>
+    );
+};
+
+const itemStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
+};
+
+export default ItemCollection;
