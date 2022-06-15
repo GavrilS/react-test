65f3dd22e0f8d5cf82d24d8b29e88f085752dd08
GAVRIL SAMODIVEKOV <gsamodivekov@gmail.com>
2022-06-15 22:19:31 +0300

    Added Home page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home'
+
 import AppState from './context/test-app/AppState';
 
 function App() {
   return (
     <AppState>
-
+      <Router>
+        <div className='App'>
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
     </AppState>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+import ItemCollection from '../searches/ItemCollection';
+
+const Home = () => {
+    <Fragment>
+        <ItemCollection />
+    </Fragment>
+};
+
+export default Home;
