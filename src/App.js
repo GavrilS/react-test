238f0745c4ec86d3f505d6553d174da3998fb7e3
GAVRIL SAMODIVEKOV <gsamodivekov@gmail.com>
2022-06-16 23:25:05 +0300

    Clear project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import React from "react";
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/pages/Home';
-
-import AppState from './context/test-app/AppState';
 
 function App() {
   return (
-    <AppState>
-      <Router>
-        <div className='App'>
-          <div className='container'>
-            <Routes>
-              <Route exact path='/' component={Home} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </AppState>
+    <div>
+      <h3>Hello world</h3>
+    </div>
   );
 }
 
