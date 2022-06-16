3c8a0a54b46918c1b4186a4cc67b8d6e5771c39e
GAVRIL SAMODIVEKOV <gsamodivekov@gmail.com>
2022-06-16 20:53:05 +0300

    Update Home component - removed syntax error

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,9 +2,11 @@ import React, { Fragment } from "react";
 import ItemCollection from '../searches/ItemCollection';
 
 const Home = () => {
-    <Fragment>
-        <ItemCollection />
-    </Fragment>
+    return (
+        <Fragment>
+            <ItemCollection />
+        </Fragment>
+    );
 };
 
 export default Home;
