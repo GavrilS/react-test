904ffd44c430682f7a043e45abe81c619ed4f167
GAVRIL SAMODIVEKOV <gsamodivekov@gmail.com>
2022-06-18 13:39:55 +0300

    Included the navbar in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <div className='container'>
         <Home />
       </div>
