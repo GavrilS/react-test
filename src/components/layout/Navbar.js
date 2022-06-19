eaa79961c0cfd120598ed7508d42bfbf93a04557
GAVRIL SAMODIVEKOV <gsamodivekov@gmail.com>
2022-06-19 13:38:19 +0300

    Changed the background color of the navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
     return (
-        <nav className="navbar bg-white">
+        <nav className="navbar bg-secondary">
             <h1>
                 <i className={icon} /> {title}
             </h1>
