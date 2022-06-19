7d8bc987d0e4da090f9eb9bddecbcbe3e20a4bdc
GAVRIL SAMODIVEKOV <gsamodivekov@gmail.com>
2022-06-19 14:43:15 +0300

    Updated App.js and ItemCollection.js to use context

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Navbar from './components/layout/Navbar';
 
+import ItemState from './context/item/ItemState';
+
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Home />
+    <ItemState>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ItemState>
   );
 }
 
